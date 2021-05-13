226a5dc1b4bae2c64319dbd4fe61577c4bc42b68
sh <61382454+sh-song@users.noreply.github.com>
2021-05-13 16:12:50 +0900

    mooyaho

--- master.py
+++ master.py
@@ -11,12 +11,11 @@ from controller import Controller
 class Master():
     
     def __init__(self):
-        pass
-        
+        self.moo = Planner()
+        self.yaho = Controller()
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(self.moo.plan(self.yaho))
+ 
 
 if __name__ == "__main__":
-    moo = Controller()
-    yaho = Planner()
-    loop = asyncio.get_event_loop()
-    
-    loop.run_until_complete(yaho.plan(moo))
+    Master()
